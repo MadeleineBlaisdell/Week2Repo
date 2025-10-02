d69b470024447290eb50b80021bbce322ecdae3a
MadeleineBlaisdell <madeleineblaisdell@gmail.com>
2025-10-01 22:12:56 -0400

    Images and visuals

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-window.onload = function () {
+window.onload = function () {//Forward. Triggers on load
   const pg2 = document.getElementById("pg2");
-  if (pg2) {
+  if (pg2) {//Checks if page 2 exists
     pg2.addEventListener("click", function () {
       window.location.href = "page2.html";
     });
   }
 
   const pg3 = document.getElementById("pg3");
-  if (pg3) {
+  if (pg3) {//Checks if page 3 exists
     pg3.addEventListener("click", function () {
       window.location.href = "page3.html";
     });
   }
 };
 
-const backBtn = document.getElementById("backBtn");
+const backBtn = document.getElementById("backBtn");//Back
   if (backBtn) {
     backBtn.addEventListener("click", function () {
-      window.history.back(); // Goes to the previous page in browser history
+      window.history.back(); //Window history tracks individual page history
     });
   };
